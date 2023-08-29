99a9d4ffe35abd134a84f34f4a145288083bec5e
Frankmona <frankoguche1@gmail.com>
2023-08-29 16:42:30 +0100

    added side bar to the responsivesness

---
+++ app.js
@@ -0,0 +1,22 @@
+// used this new method below for  sidebar toggle
+const navBar = document.querySelector(".sidebar")
+
+let toggle = true;
+const menuBtn =  document.querySelector(".nav-toggle")
+const Icon = document.querySelector(".nav-toggle i")
+
+
+menuBtn.addEventListener("click", function(){
+    // e.preventDefault();
+    if(toggle){
+        navBar.style.left = "-500px";
+        Icon.setAttribute("class", "fas fa-bars"); /*means to change the class*/
+        
+        toggle = false;
+    }else{
+        navBar.style.left = "0px";
+        Icon.setAttribute("class", "fas fa-times"); /*means to change the class*/
+        
+        toggle = true;
+    }
+});
